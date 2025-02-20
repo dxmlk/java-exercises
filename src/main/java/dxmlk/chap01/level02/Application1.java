f4a8808f0ee2fa5e5f6b986657b358240fd1bf56
dxmlk <1101sky@korea.ac.kr>
2025-02-20 15:45:48 +0900

    chap01-literal-and-variable

---
+++ src/main/java/dxmlk/chap01/level02/Application1.java
@@ -0,0 +1,18 @@
+package dxmlk.chap01.level02;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+
+        char c = 'a';
+
+        System.out.println((int)c);
+        /* 문자형 변수를 하나 선언 후 'a'를 대입한 후
+         * 출력은 문자 'a'가 가지는 유니코드값을 출력하도록 하세요
+         *
+         * -- 출력 예시 --
+         * 문자 a의 unicode : 97
+         * */
+
+    }
+}
